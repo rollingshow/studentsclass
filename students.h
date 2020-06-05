fbc9b6752a6a73a5e5b2caca84cf5f97974ee863
rollingshow <46083919+rollingshow@users.noreply.github.com>
2020-06-05 23:23:05 +0800

    Add files via upload

---
+++ students.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "human.h"
+#include <string>
+#include <vector>
+using namespace std;
+
+class student : public human 
+{
+public:
+	student(string last_name, string name, string second_name, vector<int> scores) : human(last_name, name, second_name)
+	{
+		this->scores = scores;
+	}
+
+	float get_average_score()
+	{
+		unsigned int count_scores = this->scores.size();
+		unsigned int sum_scores = 0;
+		float average_score;
+
+		for (unsigned int i = 0; i < count_scores; ++i)
+		{
+			sum_scores += this->scores[i];
+		}
+
+		average_score = (float)sum_scores / (float)count_scores;
+		return average_score;
+	}
+	
+private:
+	vector<int> scores;
+};
